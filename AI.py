3bca463831678d5cd8be1742c5d1e4980a6a4274
bargabay <bar.gabay.home@gmail.com>
2022-05-27 22:55:07 +0300

    second commit

---
+++ AI.py
@@ -0,0 +1,10 @@
+import datetime
+import threading
+
+
+# TODO: may have to get a string that will be converted from an audio.
+def calculate_sentence_score(sentence):
+
+    # TODO: The score at first is the highest (no bad things detected).
+    sentence_score = 10000
+    return sentence_score
---
+++ Main.py
@@ -0,0 +1,7 @@
+def main():
+    # TODO: change that line.
+    print(0)
+
+
+if __name__ == '__main__':
+    main()
