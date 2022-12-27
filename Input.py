77554548751ca079bf3a5a85a2e66df6a290b940
cr9169 <bar.gabay.home@gmail.com>
2022-12-27 10:29:14 +0200

    feat: bad and good words classes added, architecture changed (added AI folder)

--- Input.py
+++ Input.py
@@ -1,21 +1,41 @@
 import enchant
 
-dict = enchant.Dict("en_US")
+dictionary = enchant.Dict("en_US")
 
 
-def check_if_string_word(string, dictionary):
-    return dictionary.check(string)
+def check_if_string_word(string, d):
+    return d.check(string)
 
 
 def check_if_sentence_valid(sentence):
+    """
+    :param sentence: string.
+    :return: boolean (valid or not).
+    """
     remove_spaces_from_sentence_input(sentence)
     for word in sentence.split():
-        if not check_if_string_word(word, dict):
+        if not check_if_string_word(word, dictionary):
             return False
     return True
 
 
 def remove_spaces_from_sentence_input(sentence):
+    """
+    Ex: (" wds fsd        st          teryterre yt yty  er") =>
+    ("wds fsd st teryterre yt yty er")
+
+    :param sentence: string
+    :return: string (sentence)
+    """
+
     sentence.strip()
     sentence = " ".join(sentence.split())
     return sentence
+
+
+def main():
+    print("hi")
+
+
+if __name__ == '__main__':
+    main()
